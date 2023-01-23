9fe917877da9714f90520b6a30c1e2bb1d23c897
Girish Kumar <78135351+GKH-07@users.noreply.github.com>
2023-01-23 12:06:01 +0000

    Time: 255 ms (38.49%), Space: 70.7 MB (14.36%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        if(n==1)return   1;
+        map<int, int> mp1, mp2;
+        for(auto a: trust) {
+            mp1[a[0]]++;
+            mp2[a[1]]++;
+        }
+        // for(int i=1; i<=n; i++) {
+        //     if(mp1[i] == 0 and mp2[i] == n-1) {
+        //         return i;
+        //     }
+        // }
+        for(auto i : mp2)
+        {
+            if(i.second == n-1 && mp1[i.first] == 0)
+            {
+                return i.first;
+            }
+        }
+        return -1;
+        
+        
+    }
+};
