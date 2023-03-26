8455bb7a130855cec608e8bcd8d5060311378284
Girish Kumar <78135351+GKH-07@users.noreply.github.com>
2023-03-26 12:42:46 +0100

    Time: 0 ms (100.00%), Space: 7.2 MB (34.47%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -13,13 +13,11 @@ public:
     ListNode* middleNode(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head;
-        
-        while(fast !=NULL && fast->next!=NULL)
+        while(fast!=NULL && fast->next!=NULL)
         {
             slow = slow->next;
             fast = fast->next->next;
         }
-        
         return slow;
     }
 };
